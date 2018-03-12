4d5cce91df198606c17698e6f6184a77948e822a
Cloud_Xudell <1786546913@qq.com>
2018-03-12 22:24:19 +0800

    纯属测试！

---
+++ Prediction based on convolutional neural network/code/Data preprocessing/Splitdata.py
@@ -0,0 +1,45 @@
+import csv
+import os
+import numpy as np
+
+'''将DT.train.csv中的数据分成a_train和a_test两个csv文件，其中a_train.csv中有24500个数据，a_test.csv中有10500个数据'''
+labels = []
+data = []
+a_train_file = 'a_train.csv'
+a_test_file = 'a_test.csv'
+a_file = 'DT.Train.csv'
+
+seed = 3
+np.random.seed(seed)
+train_indices = np.random.choice(35444, 24500, replace=False)
+residue = np.array(list(set(range(35444)) - set(train_indices)))
+# print(np.array(train_indices).shape)  # (24500,)
+# print(residue.shape)  # (10944,)
+test_indices = np.random.choice(len(residue), 10500, replace=False)
+# print(np.array(test_indices).shape)  # (10500,)
+
+with open(a_file)as afile:
+    a_reader = csv.reader(afile)
+    labels = next(a_reader)
+    for row in a_reader:
+        data.append(row)
+
+# print(np.array(labels).shape)  # (24,)
+# print(np.array(data).shape)  # (35444, 24))
+
+
+# 生成训练数据集
+if not os.path.exists(a_train_file):
+    with open(a_train_file, "w", newline='') as a_trian:
+        writer = csv.writer(a_trian)
+        writer.writerows([labels])
+        writer.writerows(np.array(data)[train_indices])
+        a_trian.close()
+
+# 生成测试数据集
+if not os.path.exists(a_test_file):
+    with open(a_test_file, "w", newline='')as a_test:
+        writer = csv.writer(a_test)
+        writer.writerows([labels])
+        writer.writerows(np.array(data)[test_indices])
+        a_test.close()
---
+++ Prediction based on convolutional neural network/code/Data preprocessing/Tensorflow_LoadCSV.py
@@ -0,0 +1,57 @@
+# 使用Tensorflow读出csv文件中的数据。
+import tensorflow as tf
+
+MIN_AFTER_DEQUEUE = 1000
+BATCH_SIZE = 100
+NUM_EPOCHS = 1000
+NUM_THREADS = 2
+
+
+def read_data(file_queue):
+    reader = tf.TextLineReader(skip_header_lines=1)
+    key, value = reader.read(file_queue)
+    defaults = [[0.], [0.], [0.], [0.], [0.], [0.], [0.], [0.], [0.], [0.],
+                [0.], [0.], [0.], [0.], [0.], [0.], [0.], [0.], [0.], [0.],
+                [0.], [0.], [0.], [0.]]
+    C, MN, SI, P, S, CU, AL, ALS, NI, CR, TI, MO, V, NB, N, H, B, Furnace, RoughMill, FinishMill, DownCoil, Tensile, Yeild, Elongation \
+        = tf.decode_csv(value, defaults)
+    vertor_example = tf.stack(
+        [C, MN, SI, P, S, CU, AL, ALS, NI, CR, TI, MO, V, NB, N, H, B, Furnace, RoughMill, FinishMill,
+         DownCoil])
+    vertor_label = tf.stack([Tensile, Yeild, Elongation])
+
+    return vertor_example, vertor_label
+
+
+def create_pipeline(filename, batch_size, num_threads, num_epochs=None):
+    file_queue = tf.train.string_input_producer([filename], num_epochs=num_epochs)  # 设置文件名队列
+    example, label = read_data(file_queue)  # 读取数据和标签
+
+    min_after_dequeue = MIN_AFTER_DEQUEUE
+    # capacity = min_after_dequeue + batch_size
+    capacity = min_after_dequeue + (num_threads + 3*batch_size)
+    example_batch, label_batch = tf.train.shuffle_batch(
+        [example, label], batch_size=batch_size, num_threads=num_threads, capacity=capacity,
+        min_after_dequeue=min_after_dequeue)
+
+    return example_batch, label_batch
+
+
+x_train_batch, y_train_batch = create_pipeline('DT.Train.csv', batch_size=BATCH_SIZE, num_threads=NUM_THREADS,
+                                               num_epochs=NUM_EPOCHS)
+
+init_op = tf.global_variables_initializer()
+local_init_op = tf.local_variables_initializer()
+with tf.Session() as sess:
+    sess.run(local_init_op)
+    sess.run(init_op)
+    coord = tf.train.Coordinator()
+    threads = tf.train.start_queue_runners(coord=coord)
+    for i in range(4):
+        example, label = sess.run([x_train_batch, y_train_batch])
+        print(sess.run(tf.shape(example)))
+        print(sess.run(tf.shape(label)))
+        print(example[1][1])
+        # print(label)
+    coord.request_stop()
+    coord.join(threads)
