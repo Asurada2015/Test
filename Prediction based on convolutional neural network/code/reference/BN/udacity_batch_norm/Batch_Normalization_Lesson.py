62c70aa1150e9ad3a7d126b26634b16b8392f574
neo_cloud <1786546913@qq.com>
2018-03-16 13:57:50 +0800

    update

--- Prediction based on convolutional neural network/code/reference/BN/udacity_batch_norm/Batch_Normalization_Lesson.py
+++ Prediction based on convolutional neural network/code/reference/BN/udacity_batch_norm/Batch_Normalization_Lesson.py
@@ -315,32 +315,48 @@ class NeuralNet:
         """
         Trains a trained model on the MNIST testing dataset.
         在MNIST测试集上训练一个已经训练好的模型
+
         :param session: Session
             Used to run the testing graph operations.
+        :参数session: Session
+            用于运行测试图模型
         :param test_training_accuracy: bool (default False)
             If True, perform inference with batch normalization using batch mean and variance;
             if False, perform inference with batch normalization using estimated population mean and variance.
             Note: in real life, *always* perform inference using the population mean and variance.
                   This parameter exists just to support demonstrating what happens if you don't.
+        :参数 test_training_accuracy: bool (default False)
+        如果选择True,则会使用批数据中的平均值和方差使用Batch Normalization 算法构建前向传播预测
+        如果选择False,则会使用估计的平均值和方差来使用Batch Normalization 构建前向传播
+        注意：在实际生产中，一般使用估计的平均值和方差来进行预测数据上的前向传播，这里存在这个参数只是为了支持如果不用估计的平均值和方差的情况
         :param include_individual_predictions: bool (default True)
             This function always performs an accuracy test against the entire test set. But if this parameter
             is True, it performs an extra test, doing 200 predictions one at a time, and displays the results
             and accuracy.
+        :参数 include_individual_predictions: bool (default True)
+        此功能始终对整个测试集执行精度测试。但是，如果此参数为“真”，则会执行额外的测试，一次执行200个预测，并显示结果和准确性
+
         :param restore_from: string or None (default None)
             Name of a saved model if you want to test with previously saved weights.
+        ：参数 restore_from: string or None (default None)
+        如果要使用先前保存的权重进行测试，则可以传入模型的名称作为参数。
         """
         # This placeholder will store the true labels for each mini batch
+        # 这个占位符将存储每个小批量的真实标签
         labels = tf.placeholder(tf.float32, [None, 10])
 
         # Define operations for testing
+        # 定义测试使用的函数
         correct_prediction = tf.equal(tf.argmax(self.output_layer, 1), tf.argmax(labels, 1))
         accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
         # If provided, restore from a previously saved model
+        # 如果提供这个参数，我们会从先前保存的模型中重新加载模型
         if restore_from:
             tf.train.Saver().restore(session, restore_from)
 
         # Test against all of the MNIST test data
+        # 在所有MNIST测试图片上进行测试
         test_accuracy = session.run(accuracy, feed_dict={self.input_layer: mnist.test.images,
                                                          labels: mnist.test.labels,
                                                          self.is_training: test_training_accuracy})
@@ -348,6 +364,7 @@ class NeuralNet:
         print('{}: Accuracy on full test set = {}'.format(self.name, test_accuracy))
 
         # If requested, perform tests predicting individual values rather than batches
+        # 如果给出了include_individual_predictions参数，在独立的测试数据上测试而不是整个批次数据
         if include_individual_predictions:
             predictions = []
             correct = 0
@@ -358,6 +375,8 @@ class NeuralNet:
                 # we pass `test_training_accuracy` to `feed_dict` as the value for `self.is_training`.
                 # Remember that will tell it whether it should use the batch mean & variance or
                 # the population estimates that were calucated while training the model.
+                # 这是使用单个测试用例的正常预测。 但是，请注意我们将`test_training_accuracy`传递给`feed_dict`
+                # 作为`self.is_training`的值。 请记住，它会告诉它是否应该使用批处理平均值和方差，或者是训练模型时计算出的估计值。
                 pred, corr = session.run([tf.arg_max(self.output_layer, 1), accuracy],
                                          feed_dict={self.input_layer: [mnist.test.images[i]],
                                                     labels: [mnist.test.labels[i]],
@@ -368,3 +387,227 @@ class NeuralNet:
 
             print("200 Predictions:", predictions)
             print("Accuracy on 200 samples:", correct/200)
+
+
+# 代码中有相当多的注释，所以这些应该回答你的大部分问题。
+# 但是，让我们来看看最重要的几行。
+# 我们将批规范化添加到fully_connected函数内的图层。以下是关于该代码的一些重要观点：
+"""
+1.使用Batch Normalization的神经层不包括偏置项
+2.我们使用tf.layers.batch_normalization函数来解决数学问题(我们之后将会实现不使用这个算法的第低级版本)
+3.我们告诉tf.layers.batch_normalization网络是否正在训练。这是我们稍后谈论的重要一步。
+4.我们在调用激活函数之前使用BN规范化
+
+
+注意：我们除了使用tf.layers.batch_normalization函数之外，我们还需要使用
+with tf.control_dependencies(tf.get_collection(tf.GraphKeys.UPDATE_OPS)):
+作为声明。
+这一行实际上与我们传递给tf.layers.batch_normalization的训练参数结合使用，没有它，Tensorflow的BN层将在前向传播时无法正确运行
+
+
+最后，无论我们时训练网络或者执行前向传播推理，我们都要使用feed_dict将self.is_training 分别设置为True或False
+eg:session.run(train_step, feed_dict={self.input_layer: batch_xs, 
+                                               labels: batch_ys, 
+                                               self.is_training: True})
+                                               
+稍后我们会详细介绍这些代码，但接下来我们要展示一些使用此代码的实验，以及使用和不使用Batch Normalization进行测试的网络
+"""
+
+# Batch Normalization 使用例子
+"""
+笔记的这一部分将通过训练各种有无Batch Normalization的网络来演示前面提到的一些好处。
+我们要感谢这篇博文的作者在Tensorflow中实现Batch Normalization[https://r2rt.com/implementing-batch-normalization-in-tensorflow.html]
+这篇文章提供了一些想法和一些代码来绘制训练期间准确度的差异和使用相同的权重来初始化多个不同网络的想法。
+"""
+
+# 支持测试的代码
+"""
+以下两个函数方法支持我们在notebook中运行的例子
+
+第一个函数 plot_training_accuracies,简单的绘制了NeuralNet类传递给training_accuracies列表的精确度数值
+如果你看了NeuralNet中的训练功能，你会发现它在训练网络时会定期测量验证的准确性并将结果存储在该列表中。
+
+第二个函数train_and_test创建了两个神经网络 一个使用了Batch Normalization和一个没有Batch Normalization。然后训练它们并对它们进行测试，
+调用plot_training_accuracies函数来绘制它们在训练过程中的准确度如何变化。train_and_test函数真正重要的一点是，它初始化网络外部神经元层的起始权重，然后将它们传入。
+这使得它可以从完全相同的起始权重训练两个网络，这消除了由不同初始权重导致的性能差异。
+"""
+
+
+def plot_training_accuracies(*args, **kwargs):
+    """
+    Displays a plot of the accuracies calculated during training to demonstrate
+    how many iterations it took for the model(s) to converge.
+    显示训练期间计算的精度图，以显示模型收敛需要多少次迭代。
+
+    :param args: One or more NeuralNet objects
+        You can supply any number of NeuralNet objects as unnamed arguments
+        and this will display their training accuracies. Be sure to call `train`
+        the NeuralNets before calling this function.
+    :参数 args: One or more NeuralNet objects
+    你可以提供任意数量的NeuralNet对象作为未命名的参数，这将显示他们的训练精度。在调用这个函数之前，一定要调用`训练'NeuralNets'对象。
+
+    :param kwargs:
+        You can supply any named parameters here, but `batches_per_sample` is the only
+        one we look for. It should match the `batches_per_sample` value you passed
+        to the `train` function.
+    :param kwargs:
+    你可以在这里提供任何命名参数，但`batches_per_sample`是唯一的我们寻找的一个。
+    它应该与您传递的`batches_per_sample`值相匹配到"训练"功能。
+    """
+    fig, ax = plt.subplots()
+
+    batches_per_sample = kwargs['batches_per_sample']
+
+    for nn in args:
+        ax.plot(range(0, len(nn.training_accuracies)*batches_per_sample, batches_per_sample),
+                nn.training_accuracies, label=nn.name)
+    ax.set_xlabel('Training steps')
+    ax.set_ylabel('Accuracy')
+    ax.set_title('Validation Accuracy During Training')
+    ax.legend(loc=4)
+    ax.set_ylim([0, 1])
+    plt.yticks(np.arange(0, 1.1, 0.1))
+    plt.grid(True)
+    plt.show()
+
+
+def train_and_test(use_bad_weights, learning_rate, activation_fn, training_batches=50000, batches_per_sample=500):
+    """
+    Creates two networks, one with and one without batch normalization, then trains them
+    with identical starting weights, layers, batches, etc. Finally tests and plots their accuracies.
+    创建两个神经网络，一个使用Batch Normalization 一个不使用Batch Normalization
+    然后使用相同的初始权重，层，批次量等等，最后检测并画出精确度曲线
+
+    :param use_bad_weights: bool
+        If True, initialize the weights of both networks to wildly inappropriate weights;
+        if False, use reasonable starting weights.
+    :参数 use_bad_weights: bool
+        如果为True，则将两个网络的权重初始化为非常不恰当的权重;
+        如果是False，则使用合理的起始权重。
+
+    :param learning_rate: float
+        Learning rate used during gradient descent.
+    :参数 学习率：float
+        梯度下降时使用的学习率
+
+    :param activation_fn: Callable
+        The function used for the output of each hidden layer. The network will use the same
+        activation function on every hidden layer and no activate function on the output layer.
+        e.g. Pass tf.nn.relu to use ReLU activations on your hidden layers.
+     :参数 激活函数: 可调用
+        对隐藏层输出执行的函数，在每一个隐层会使用相同的激活函数，但是在最后一层不使用激活函数。
+        eg: 通过传入tf.nn.relu在隐藏层使用ReLU激活函数
+
+    :param training_batches: (default 50000)
+        Number of batches to train.
+     :参数 training_batches: (default 50000)
+        训练批次数
+    :param batches_per_sample: (default 500)
+     :参数 batches_per_sample: (default 500)
+        How many batches to train before sampling the validation accuracy.
+        每训练batches_per_sample后使用验证集计算准确率
+    """
+    # Use identical starting weights for each network to eliminate differences in
+    # weight initialization as a cause for differences seen in training performance
+    # 为每个网络使用相同的起始权重以消除权重初始化中的差异，这因为权重初始化是造成培训性能差异的一个重要原因
+    # Note: The networks will use these weights to define the number of and shapes of
+    #       its layers. The original batch normalization paper used 3 hidden layers
+    #       with 100 nodes in each, followed by a 10 node output layer. These values
+    #       build such a network, but feel free to experiment with different choices.
+    #       However, the input size should always be 784 and the final output should be 10.
+    # 注意：网络将使用这些权重来定义神经网络模型的形状,原始Batch Normalization模型使使用3个隐藏层
+    #      每个节点有100个节点，后面是10个节点输出层。你也可以建立这样一个网络，并随时尝试不同的选择。
+    #      但是，输入大小应始终为784，最终输出应为10
+    if use_bad_weights:
+        # These weights should be horrible because they have such a large standard deviation
+        # 这些数值很坏因为他们具有十分大的标准差
+        weights = [np.random.normal(size=(784, 100), scale=5.0).astype(np.float32),
+                   np.random.normal(size=(100, 100), scale=5.0).astype(np.float32),
+                   np.random.normal(size=(100, 100), scale=5.0).astype(np.float32),
+                   np.random.normal(size=(100, 10), scale=5.0).astype(np.float32)
+                   ]
+    else:
+        # These weights should be good because they have such a small standard deviation
+        # 这些初始化权重很好，因为它们具有很小的标准差
+        weights = [np.random.normal(size=(784, 100), scale=0.05).astype(np.float32),
+                   np.random.normal(size=(100, 100), scale=0.05).astype(np.float32),
+                   np.random.normal(size=(100, 100), scale=0.05).astype(np.float32),
+                   np.random.normal(size=(100, 10), scale=0.05).astype(np.float32)
+                   ]
+
+    # Just to make sure the TensorFlow's default graph is empty before we start another
+    # test, because we don't bother using different graphs or scoping and naming
+    # elements carefully in this sample code.
+    # 在我们开始另一个测试时我们要确保Tensorflow的默认图模型是空的，因为我们不会在这个示例代码中仔细地使用不同的图或范围来命名元素。
+    tf.reset_default_graph()
+
+    # build two versions of same network, 1 without and 1 with batch normalization
+    # 建立两个不同版本的相同神经网络，一个使用Batch Normalization 另一个不使用
+    nn = NeuralNet(weights, activation_fn, False)
+    bn = NeuralNet(weights, activation_fn, True)
+
+    # train and test the two models
+    # 训练并测试两个模型
+    with tf.Session() as sess:
+        tf.global_variables_initializer().run()
+
+        nn.train(sess, learning_rate, training_batches, batches_per_sample)
+        bn.train(sess, learning_rate, training_batches, batches_per_sample)
+
+        nn.test(sess)
+        bn.test(sess)
+
+    # Display a graph of how validation accuracies changed during training so we can compare
+    #  how the models trained and when they converged
+    # 显示培训期间验证准确度如何变化的图形，以便我们可以比较模型的训练和收敛时间
+    plot_training_accuracies(nn, bn, batches_per_sample=batches_per_sample)
+
+
+# 下一系列单元通过各种设置来训练网络，以显示有无Batch Normalization的差异。它们旨在清楚地表明批量标准化的效果。
+# 下面使用ReLU激活函数创建两个网络，学习率为0.01，并且启动权重合理。
+
+
+# train_and_test(False, 0.01, tf.nn.relu)
+
+
+# 100%|██████████| 50000/50000 [01:25<00:00, 587.98it/s]
+# Without Batch Norm: After training, final accuracy on validation set = 0.9742000102996826
+# 100%|██████████| 50000/50000 [02:29<00:00, 333.88it/s]
+# With Batch Norm: After training, final accuracy on validation set = 0.980400025844574
+# ---------------------------------------------------------------------------
+# Without Batch Norm: Accuracy on full test set = 0.9739000201225281
+# ---------------------------------------------------------------------------
+# With Batch Norm: Accuracy on full test set = 0.9786999821662903
+"""
+图片保存为:train_and_test_ReLU50000.png
+如预期的那样，两个网络训练良好并最终达到相似的测试精度。但是，请注意，批量归一化模型的收敛速度比其他网络要快一些，
+几乎立即达到90％以上的准确度，并在10或15000次迭代中接近最大准确度。另一个网络需要大约3千次迭代才能达到90％，并且在3万次或更多迭代之前不会达到最佳精度。
+
+如果您查看原始速度，您可以看到，如果没有批量标准化，我们每秒计算超过1100次迭代，而批量标准化会降低到500个以上。
+然而，批量标准化允许我们执行更少的迭代并且收敛时需要更少的时间。（我们只在这里培训了5万迭代，所以我们可以绘制比较结果。）
+"""
+
+# 以下示例创建了两个神经网络，它们使用前面示例中使用的相同的参数，但只有2000次迭代的训练
+train_and_test(False, 0.01, tf.nn.relu, 2000, 50)
+
+
+# 图片保存为train_and_test_2000.png
+# 100%|██████████| 2000/2000 [00:04<00:00, 478.34it/s]
+# Without Batch Norm: After training, final accuracy on validation set = 0.8378000259399414
+# 100%|██████████| 2000/2000 [00:07<00:00, 273.43it/s]
+# With Batch Norm: After training, final accuracy on validation set = 0.953000009059906
+# ---------------------------------------------------------------------------
+# Without Batch Norm: Accuracy on full test set = 0.8328999876976013
+# ---------------------------------------------------------------------------
+# With Batch Norm: Accuracy on full test set = 0.9487000107765198
+"""
+正如你所看到的，使用批量标准化生成的模型只运行2000个迭代次数精度超过了95％，而在大约500个迭代的某处达到了90％以上。
+如果没有批量标准化，模型需要1750次迭代才能达到80％ - 批量标准化的网络在经过大约200次迭代后就会达到该标记！ 
+(注意：你自己在运行时，尽管模型每次的权重初始化相同，但是模型每次运行时生成的随机数不同)。 
+
+在上面的例子中，你也应该注意到网络每秒训练的批次数少于你在前面的例子中看到的。
+这是因为我们正在追踪的大部分时间实际上都是周期性地执行inference并收集用以显示的数据的过程。(inference是很花时间的)
+在这个例子中，我们每50个批次执行一次inference，而不是每500个进行一次inference(前向传播预测)，
+因此这个例子相对于上个模型生成2000次的图，时间的花销提高了十倍
+因此为这个例子生成图需要相同2000次迭代的开销的10倍。
+"""
